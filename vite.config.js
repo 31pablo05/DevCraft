8e8a03d6a33dc86c12d59749ff6bc71fea531621
31pablo05 <Pabloproboste64@gmail.com>
2025-10-01 16:40:17 -0300

    ajustes para arreglar visualiozacion web

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   server: {
     port: 5174,
   },
