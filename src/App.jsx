61d95cbcaa414e7a3b4f4d5f9e43efc56562f600
31pablo05 <Pabloproboste64@gmail.com>
2025-10-02 10:16:13 -0300

    ajuste en scroll automatico

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useEffect } from 'react'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
@@ -10,6 +11,13 @@ import Footer from './components/Footer'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    if (window.location.hash) {
+      window.location.hash = ''
+    }
+  }, [])
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-100">
       <Header />
